a710ce4221c3d15bd9c0e6f7d7c02d578006ef85
KevinNalli <kncarrascalg@ufpso.edu.co>
2024-08-29 21:25:09 -0500

    jayyyy

--- Ejercicio_Matrices/src/main/java/com/mycompany/ejercicio_matrices/Menu.java
+++ Ejercicio_Matrices/src/main/java/com/mycompany/ejercicio_matrices/Menu.java
@@ -4,32 +4,35 @@ package com.mycompany.ejercicio_matrices;
 import javax.swing.JOptionPane;
 
 public class Menu {
-    String platos[];
+    Plato menu[];
     int num_platos[][];
     String semana[] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
     int[] ventas_dia = new int[6];
     
     public void crearMenu(int cantidad){
-        platos = new String[cantidad];
+        menu = new Plato[cantidad];
         int i = 0;
         while(i<cantidad){
-            platos[i]= JOptionPane.showInputDialog("Diga el nombre del plato "+(i+1));
+            menu[i] = new Plato();
+            menu[i].setName(JOptionPane.showInputDialog("Diga el nombre del plato "+(i+1)));
+            menu[i].setPrice(Integer.parseInt(JOptionPane.showInputDialog(null,"Diga el precio del plato "+(i+1))));
+            menu[i].setIngredients(JOptionPane.showInputDialog("Diga los ingredientes "+(i+1)));
             i++;
         }
     }
     public void consultarMenu(){
         String x = "";
-        for(int i = 0; i<platos.length ; i++){
-            x = x +platos[i]+"\n";
+        for(int i = 0; i<menu.length ; i++){
+            x = x+ menu[i].getName()+": "+menu[i].getIngredients()+": $"+menu[i].getPrice()+"\n";
         }
-        JOptionPane.showMessageDialog(null, " Platos:  \n"+x);
+        JOptionPane.showMessageDialog(null, " Platos:  \n \n"+x);
     }
     
     public void ingresarDatos(){
-        num_platos = new int[6][platos.length];
+        num_platos = new int[6][menu.length];
         for(int i = 0; i<semana.length; i++){
-            for(int j = 0; j<platos.length;j++){
-                num_platos[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Ventas para el dia "+semana[i]+ " Plato: "+ platos[j]));
+            for(int j = 0; j<menu.length;j++){
+                num_platos[i][j] = Integer.parseInt(JOptionPane.showInputDialog(null, "Ventas para el dia "+semana[i]+ " Plato: "+ menu[j].getName()));
                 ventas_dia[i] += num_platos[i][j];
             }
         }
@@ -37,20 +40,44 @@ public class Menu {
     }
     public void analizar(){
         int best_day = 0;
+        int bad_day = 1000000;
         int total_ventas = 0;
-        int[] platoss = new int[platos.length];
+        int[] venta_plato = new int[menu.length];
         for(int i = 0; i<semana.length; i++){
-            for(int j = 0; j<platos.length;j++){
-                platoss[j] = platoss[j]+ num_platos[i][j];
+            for(int j = 0; j<menu.length;j++){
+                venta_plato[j] = venta_plato[j]+ num_platos[i][j];
                 if(ventas_dia[i]>best_day){
                     best_day = i;
                 }
+                if(ventas_dia[i]<bad_day){
+                    bad_day = ventas_dia[i];
+                }
             }
-            total_ventas += platoss[i];
+            
+        }
+        int best_plate = venta_plato[0];
+        int best = 0;
+        int bad_plate = venta_plato[0];
+        int bad = 0;
+        for(int i = 0; i< venta_plato.length; i++){
+            if(venta_plato[i]>best_plate){
+                best_plate = venta_plato[i];
+                best = i;
+            }
+            if(venta_plato[i]<bad_plate){
+                bad_plate = venta_plato[i];
+                bad = i;
+            }
+            total_ventas += venta_plato[i];
+            
         }
         
         JOptionPane.showMessageDialog(null, "Numero de platos vendidos: "+ total_ventas +"\n" 
-            + "Dia mas vendido: "+ semana[best_day]+"\n"+"Con "+ventas_dia[best_day] +" platos" +"\n"
+            + "Dia mas vendido: "+ semana[best_day]+"\n"
+            +"Con "+ventas_dia[best_day] +" platos" +"\n"
+            + "Dia menos vendido: "+ semana[bad_day]+"\n"
+            +"Con "+ventas_dia[bad_day] +" platos" +"\n"
+            + "Plato mas vendido: "+ menu[best].getName()+ " con "+venta_plato[best]+" platos"+"\n"
         );
     }
 }
---
+++ Ejercicio_Matrices/src/main/java/com/mycompany/ejercicio_matrices/Plato.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.ejercicio_matrices;
+
+public class Plato {
+    private String Name;
+    private int price;
+    private String ingredients;
+
+    public Plato() {
+    }
+    
+    public String getName() {
+        return Name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+     
+    
+    
+}
